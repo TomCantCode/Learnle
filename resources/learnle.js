3e3035b94a7f5e13439e0735377736a74a6a6e98
NaeNaePekka <18jonesa@cedarsupper.co.uk>
2022-11-13 18:48:47 +0000

    missed N

--- resources/learnle.js
+++ resources/learnle.js
@@ -69,8 +69,8 @@ function grid_add_string(name, string, row) {
 
   if (GUESSES[GUESSNUM].replace(undefined, '') == ANSWER || GUESSNUM == (MAXGUESSES - 1)) {
     FREEZE = true;
+    
     reset_grid('grid-container');
-    alert('yes');
   };
 
 };
@@ -82,7 +82,7 @@ function end() {
 
 var squareNum = 0;
 const legalInputs =
-  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
 window.addEventListener('keydown', function(event) {
