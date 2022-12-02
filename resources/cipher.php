227b09708a2cbc85f61247b1c070f6106bc66044
Alfie Jones <101135270+NaeNaePekka@users.noreply.github.com>
2022-12-02 10:10:25 +0000

    cipher in php

--- resources/cipher.php
+++ resources/cipher.php
@@ -1,46 +1,47 @@
 <?php
 function Encrypt($string) {
 	
-    //Creates a key the same length as the input string
+    //Creates a random key the same length as the input string
     $key = [];
-    for ($a = 0; $password; ) {
+    for ($a = 0; $a == strlen($string); $a++) {
         $key[substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 1)];
     }
+    $key = implode('', $key);
 	
 
     //Set variables
-    $Encrypted = [];
-    $keyBinary = list(key);
-    $toEncryptBinary = list(password);
-    $EncryptedBinary = [];
-    $EncryptedBinarya = [];
-    $EncryptedBinaryb = [];
+    $Encrypted = array();
+    $keyBinary = str_split($key);
+    $toEncryptBinary = str_split($string);
+    $EncryptedBinary = array();
+    $EncryptedBinarya = array();
+    $EncryptedBinaryb = array();
 
-    FOR k = 0 TO len(password):
-        keyBinary[k] = BINARY(ASCII(keyBinary[k]))
-        toEncryptBinary[k] = BINARY(ASCII(toEncryptBinary[k]))
-    ENDFOR	
+    for($b = 0; $b == strlen($string); $b++) {
+        $keyBinary[$b] = decbin(ord($keyBinary[$b]));
+        $toEncryptBinary[$b] = decbin(chr($toEncryptBinary[$b]));
+    }
 
-    FOR i = 0 TO len(password):
-        FOR j = 0 TO len(toEncryptBinary[i]):
-            EncryptedBinarya.APPEND(
-            str(keyBinary[i][j] ^ toEncryptBinary[i][j]))
+    for($c = 0; $c == strlen($string); $c++) {
+        for($d = 0; $d == strlen($toEncryptBinary[$c]); $d++) {
+            array_push($EncryptedBinarya, strval($keyBinary[$c][$d] xor $toEncryptBinary[$c][$d]));
 
-            EncryptedBinaryb.APPEND(EncryptedBinarya)
-            EncryptedBinarya = []
-
-	  ENDFOR
-
-        Encrypted.APPEND(ASCII(DENARY(''.JOIN(EncryptedBinaryb))))
-        EncryptedBinaryb = []
-
-    ENDFOR
-
-    EncryptedBinary.APPEND(EncryptedBinaryb)
+            array_push($EncryptedBinaryb, $EncryptedBinarya);
+            $EncryptedBinarya = array();    
+        }
     
 
-    half = ROUND(len(key)/2)
-    enPassword = key[:half] + Encrypted + key[half + 1:]
-    RETURN enPassword
+        array_push($Encrypted, chr(bindec(implode('', $EncryptedBinaryb))));
+        $EncryptedBinaryb = array();
+
+    }
+
+    array_push($EncryptedBinary, $EncryptedBinaryb);
+    
+    //Adds the key to encrypted password by 
+    $half = floor(strlen($key)/2);
+    $enPassword = substr($key, $half) + $Encrypted + substr($key, $half + 1);
+    return $enPassword;
 }
+
 ?>
